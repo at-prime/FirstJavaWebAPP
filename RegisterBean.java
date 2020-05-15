10f45c9df61fc5c19149b4fd075d6f28679ad02e
at-prime <65404570+at-prime@users.noreply.github.com>
2020-05-15 20:36:01 +0530

    Add files via upload

---
+++ RegisterBean.java
@@ -0,0 +1,74 @@
+package com.mvc.dao;
+ 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import com.mvc.bean.RegisterBean;
+import com.mvc.util.DBConnection;
+ 
+public class RegisterDao { 
+     public String registerUser(RegisterBean registerBean)
+     {
+         String fullName = registerBean.getFullName();
+         String email = registerBean.getEmail();
+         String userNpackage com.mvc.bean;
+ 
+public class RegisterBean {
+ 
+ private String fullName;
+ private String email;
+ private String userName;
+ private String password;
+ 
+ public String getUserName() {
+ return userName;
+ }
+ public void setUserName(String userName) {
+ this.userName = userName;
+ }
+ public String getPassword() {
+ return password;
+ }
+ public void setPassword(String password) {
+ this.password = password;
+ }
+ public void setFullName(String fullName) {
+ this.fullName = fullName;
+ }
+ public String getFullName() {
+ return fullName;
+ }
+ public void setEmail(String email) {
+ this.email = email;
+ }
+ public String getEmail() {
+ return email;
+ }
+}
+ame = registerBean.getUserName();
+         String password = registerBean.getPassword();
+         
+         Connection con = null;
+         PreparedStatement preparedStatement = null;         
+         try
+         {
+             con = DBConnection.createConnection();
+             String query = "insert into users(SlNo,fullName,Email,userName,password) values (NULL,?,?,?,?)"; 
+             preparedStatement = con.prepareStatement(query); 
+             preparedStatement.setString(1, fullName);
+             preparedStatement.setString(2, email);
+             preparedStatement.setString(3, userName);
+             preparedStatement.setString(4, password);
+             
+             int i= preparedStatement.executeUpdate();
+             
+             if (i!=0)  
+             return "SUCCESS"; 
+         }
+         catch(SQLException e)
+         {
+            e.printStackTrace();
+         }       
+         return "Oops.. Something went wrong there..!";  
+     }
+}
